3c0559529be5003c7aa583774e3a19efaa7124ee
Hendi Santika <hendisantika@gmail.com>
2017-11-02 22:03:53 +0700

    #3 - Tambah buatcsv.py

---
+++ buatcsv.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python
+#import csv
+
+lines=open('List_Order.txt').readlines()[8:-1]
+
+total=0
+
+for row in lines:
+   rsplit=row.split(',')
+   total=total+int(rsplit[2])
+
+print 'Total:',total
+
+open('orders.csv', 'w').writelines(lines)
